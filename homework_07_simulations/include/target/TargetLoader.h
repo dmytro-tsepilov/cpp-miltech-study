c472c68098732416381ac2c30756d97e8addc123
Dmytro Tsepilov <dmytro.tsepilov@gmail.com>
2026-05-23 22:30:09 +0300

    Created fabric for ITargetProvider

--- homework_07_simulations/include/target/TargetLoader.h
+++ homework_07_simulations/include/target/TargetLoader.h
@@ -1,8 +1,9 @@
-#include <ostream>
 #include <string>
 #include "json.hpp"
 
 #include "drone/DroneConfig.h"
+
+// ============ ITargetProvider Interface ============
 
 class ITargetProvider {
 public:
@@ -13,6 +14,8 @@ public:
     virtual Target *getTarget(int index) = 0;
 };
 
+// ============ JsonTargetProvider ============
+
 class JsonTargetProvider : public ITargetProvider {
 private:
     std::string fileName;
@@ -22,14 +25,14 @@ private:
     Target** targets = nullptr;
 
 public:
-    JsonTargetProvider(const std::string &filename = "targets.json") {
-        fileName = filename;
+    JsonTargetProvider(const std::string &folderPath = "", const std::string &filename = "targets.json") {
+        this->fileName = filename;
+        this->folderPath = folderPath;
     }
 
     bool load() override;
     int getTargetCount() override;
     int getTimeSteps() override;
-    bool loadTargetsFromFile();
     Target *getTarget(int index) override;
     void setFolderPath(const std::string folderPath = "");
 
@@ -41,3 +44,75 @@ public:
         targets = nullptr;
     }
 };
+
+// ============ SerialTargetProvider ============
+
+class SerialTargetProvider : public ITargetProvider {
+private:
+    std::string portName;
+    int targetCount;
+    int timeSteps;
+    Target** targets = nullptr;
+
+public:
+    SerialTargetProvider(const std::string &port);
+
+    bool load() override;
+    int getTargetCount() override;
+    int getTimeSteps() override;
+    Target *getTarget(int index) override;
+
+    ~SerialTargetProvider() override
+    {
+        for (int i = 0; i < targetCount; i++)
+            delete[] targets[i];
+        delete[] targets;
+        targets = nullptr;
+    }
+};
+
+// ============ TestTargetProvider ============
+
+class TestTargetProvider : public ITargetProvider {
+private:
+    int targetCount;
+    int timeSteps;
+    Target** targets = nullptr;
+
+public:
+    TestTargetProvider();
+
+    bool load() override;
+    int getTargetCount() override;
+    int getTimeSteps() override;
+    Target *getTarget(int index) override;
+
+    ~TestTargetProvider() override
+    {
+        for (int i = 0; i < targetCount; i++)
+            delete[] targets[i];
+        delete[] targets;
+        targets = nullptr;
+    }
+};
+
+// ============ Factory Function ============
+
+enum class SourceType {
+    JSON,
+    SERIAL,
+    TEST
+};
+
+inline ITargetProvider* createTargetProvider(SourceType type, const char* param = nullptr, const char* param2 = nullptr) {
+    switch (type) {
+        case SourceType::JSON:
+            return new JsonTargetProvider(param ? param : "", param2 ? param2 : "targets.json");
+        case SourceType::SERIAL:
+            return new SerialTargetProvider(param ? param : "/dev/ttyUSB0");
+        case SourceType::TEST:
+            return new TestTargetProvider();
+        default:
+            return nullptr;
+    }
+}
