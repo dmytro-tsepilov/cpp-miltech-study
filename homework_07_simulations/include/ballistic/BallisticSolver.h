d0c7f44a86cffba85a726d3c30399865286cdc4e
Dmytro Tsepilov <dmytro.tsepilov@gmail.com>
2026-05-24 23:03:10 +0300

    Minor fixes for AnalyticalSolver

--- homework_07_simulations/include/ballistic/BallisticSolver.h
+++ homework_07_simulations/include/ballistic/BallisticSolver.h
@@ -6,13 +6,13 @@ class IBallisticSolver {
 public:
     virtual ~IBallisticSolver() = default;
     virtual double calculateHorizontalDistance(const float &attackSpeed, const float &ammoDrag, const float &ammoMass, const float &ammoLift, const double &time) = 0;
-    virtual double calculateTimeToTarget(const float attackSpeed, const float ammoDrag, const float ammoMass, const float ammoLift, const float zd) = 0;
+    virtual double calculateTimeToTarget(const float &attackSpeed, const float &ammoDrag, const float &ammoMass, const float &ammoLift, const float &zd) = 0;
 };
 
 class AnalyticalSolver : public IBallisticSolver {
 public:
     double calculateHorizontalDistance(const float &attackSpeed, const float &ammoDrag, const float &ammoMass, const float &ammoLift, const double &time) override;
-    double calculateTimeToTarget(const float attackSpeed, const float ammoDrag, const float ammoMass, const float ammoLift, const float zd) override;
+    double calculateTimeToTarget(const float &attackSpeed, const float &ammoDrag, const float &ammoMass, const float &ammoLift, const float &zd) override;
 };
 
 // ============ IBallisticSolver Factory Function ============
